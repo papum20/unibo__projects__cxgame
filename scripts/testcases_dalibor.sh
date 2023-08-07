346439feec79d23be2ace8fe93ec8f654498aa8c
daniele.dugo <daniele.dugo@studio.unibo.it>
2023-08-07 23:52:54 +0200

    ?

--- scripts/testcases_dalibor.sh
+++ scripts/testcases_dalibor.sh
@@ -34,19 +34,19 @@ cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_nonum_995.txt
 ./ltest 9 9 5		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.halfnPlayer			-r ${TESTS} > ${OUTDIR}/ranch_halfn_995.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_halfn_995.txt
 
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nocel.Player				-r ${TESTS} > ${OUTDIR}/ranch_nocel_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nocel.Player				-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_nocel_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_nocel_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nocel.nonmc.Player			-r ${TESTS} > ${OUTDIR}/ranch_nonmc_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nocel.nonmc.Player			-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_nonmc_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_nonmc_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nocel.nonmc.tryit.Player	-r ${TESTS} > ${OUTDIR}/ranch_tryit_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nocel.nonmc.tryit.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_tryit_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_tryit_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nonmc.Player				-r ${TESTS} > ${OUTDIR}/ranch_nomc0_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nonmc.Player				-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_nomc0_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_nomc0_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nonmc.tryit.Player			-r ${TESTS} > ${OUTDIR}/ranch_tryt0_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.nonmc.tryit.Player			-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_tryt0_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_tryt0_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.Player				-r ${TESTS} > ${OUTDIR}/ranch_alpha_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.Player				-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_alpha_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_alpha_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.nonum.Player			-r ${TESTS} > ${OUTDIR}/ranch_nonum_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.nonum.Player			-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_nonum_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_nonum_18147.txt
-./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.halfnPlayer			-r ${TESTS} > ${OUTDIR}/ranch_halfn_18147.txt
+./ltest 18 14 7		pndb.nocel.nonmc.tryit.ranch.Player pndb.alpha.halfnPlayer			-r ${TESTS} | tail -40 > ${OUTDIR}/ranch_halfn_18147.txt
 cat scripts/testcases_dalibor.sh >> ${OUTDIR}/ranch_halfn_18147.txt
--- scripts/testcases_moschina.sh
+++ scripts/testcases_moschina.sh
@@ -29,16 +29,16 @@ cat ./scripts/testcases_moschina >> nonum_ranch_995.txt
 ./ltest 9 9 5	pndb.alpha.halfn.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/halfn_ranch_995.txt
 cat ./scripts/testcases_moschina >> halfn_ranch_995.txt
 
-./ltest 18 22 7	pndb.alpha.Player				pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/alpha_ranch_18227.txt
+./ltest 18 22 7	pndb.alpha.Player				pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/alpha_ranch_18227.txt
 cat ./scripts/testcases_moschina >> alpha_ranch_995.txt
-./ltest 18 22 7	pndb.nocel.Player				pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/nocel_ranch_18227.txt
+./ltest 18 22 7	pndb.nocel.Player				pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/nocel_ranch_18227.txt
 cat ./scripts/testcases_moschina >> nocel_ranch_995.txt
-./ltest 18 22 7	pndb.nocel.nonmc.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/nonmc_ranch_18227.txt
+./ltest 18 22 7	pndb.nocel.nonmc.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/nonmc_ranch_18227.txt
 cat ./scripts/testcases_moschina >> nonmc_ranch_995.txt
-./ltest 18 22 7	pndb.nocel.nonmc.tryit.Player	pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/tryit_ranch_18227.txt
+./ltest 18 22 7	pndb.nocel.nonmc.tryit.Player	pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/tryit_ranch_18227.txt
 cat ./scripts/testcases_moschina >> tryit_ranch_995.txt
-./ltest 18 22 7	pndb.alpha.nonum.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/nonum_ranch_18227.txt
+./ltest 18 22 7	pndb.alpha.nonum.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/nonum_ranch_18227.txt
 cat ./scripts/testcases_moschina >> nonum_ranch_995.txt
-./ltest 18 22 7	pndb.alpha.halfn.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} > ${OUTDIR}/halfn_ranch_18227.txt
+./ltest 18 22 7	pndb.alpha.halfn.Player			pndb.nocel.nonmc.tryit.ranch.Player	-r ${TESTS} | tail -40 > ${OUTDIR}/halfn_ranch_18227.txt
 cat ./scripts/testcases_moschina >> halfn_ranch_995.txt
 
